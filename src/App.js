06cee20f741f6addbee9d49db0dab611471a7882
dsuccar <danielsuccar@icloud.com>
2020-11-30 16:58:38 -0500

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react'
+import {Route, Switch, withRouter} from "react-router-dom"
 import './App.css';
+// import AboutPage from "./components/AboutPage"
+// import Navbar from "./components/Navbar";
+import Discover from "./components/Discover"
+// import {connect} from 'react-redux'
+// import {fetchingBreweries, fetchingReviews, fetchingUsers} from './Redux/actions'
 
-function App() {
+
+
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      breweries: {}
+    }
+  }
+  fetchBreweries = () => {
+    fetch(`http://localhost:3000/breweries`)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        breweries: res
+      })
+    })
+  }
+
+
+  // const [breweries, setBreweries] = useState([])
+  // const [users, setUsers] = useState([])
+
+  // const getBreweries = () => {
+   
+  //   fetch(`http://localhost:3000/breweries`)
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     setBreweries(res)
+  //   })
+  // }
+
+  // const getReviews = () => {
+   
+  //   fetch(`http://localhost:3000/reviews`)
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     console.log(res)
+  //   })
+  // }
+
+  // const getUsers = () => {
+   
+  //   fetch(`http://localhost:3000/users`)
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     console.log(res)
+  //   })
+  // }
+
+  // useEffect(getBreweries, [])
+
+render(){
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+    {/* <Navbar/> */}
+      {/* <Switch> */}
+       <Discover fetchBreweries={this.fetchBreweries}/>
+      
+        {/* <Route   path="/" component={BreweriesContainer} /> */}
+      {/* </Switch> */}
+      
+   </div>
   );
 }
+}
 
-export default App;
+ 
---
+++ src/components/Discover.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+
+
+
+export default function Discover() {
+
+  
+debugger
+
+  return (
+   <div >
+<button> hey there</button>      
+   </div>
+  );
+}
+
+ 
